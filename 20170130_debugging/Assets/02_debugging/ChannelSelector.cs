e2842d5284addf7f49dbd773b2b35d5a8bd5f3af
Kurtina <kurtdekker@gmail.com>
2017-01-28 12:00:38 -0800

    debugging talk - breaking endless loops

---
+++ 20170130_debugging/Assets/02_debugging/ChannelSelector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChannelSelector : MonoBehaviour
+{
+	int currentChannel;
+
+	public Text TextCurrentChannel;
+
+	void UpdateTextCurrentChannel()
+	{
+		TextCurrentChannel.text = "Current Channel: " + currentChannel.ToString ();
+	}
+
+	public void ChangeChannel( int direction)
+	{
+		currentChannel += direction;
+
+		UpdateTextCurrentChannel ();
+	}
+
+	void Start ()
+	{
+		UpdateTextCurrentChannel ();
+	}
+}
---
+++ 20170130_debugging/Assets/02_debugging/EndlessLooping.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndlessLooping : MonoBehaviour
+{
+	void Start ()
+	{
+		QuickLabel.Create ("Time: ", () =>
+		{
+			return Time.time.ToString ("0.0"); });
+	}
+
+	public void BeginMyEndlessIntegerLoop()
+	{
+		int j = 0;
+		
+		for (int i = 0; i != 100; )
+		{
+			j += i;
+		}
+	}
+
+	public void BeginMyEndlessSearchLoop()
+	{
+		int i = 123;
+		bool done = false;
+		while (!done)
+		{
+			int j = Random.Range ( 0, 100);
+			if (j == i)
+			{
+				done = true;
+			}
+		}
+	}
+}
