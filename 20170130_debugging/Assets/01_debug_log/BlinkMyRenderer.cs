4822c28735bb51b4be312046c57c4e6fe896bf00
Kurtina <kurtdekker@gmail.com>
2017-01-28 10:34:17 -0800

    01_debug_log scene fleshed out

---
+++ 20170130_debugging/Assets/01_debug_log/BlinkMyRenderer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkMyRenderer : MonoBehaviour
+{
+	void Update ()
+	{
+		bool onoff = (Time.time % 1.0f) < 0.5f;
+
+		gameObject.GetComponent<Renderer> ().enabled = onoff;
+	}
+}
