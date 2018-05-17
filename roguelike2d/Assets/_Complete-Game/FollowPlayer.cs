64061f739d3e0beea382bf695a1a17a73fe1709c
Kurt Dekker <kdekker@farcaster>
2018-05-17 11:29:16 -0700

    Follow the player around with the camera

---
+++ roguelike2d/Assets/_Complete-Game/FollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// put this on the camera to track the player (keep the player centered)
+
+public class FollowPlayer : MonoBehaviour
+{
+	GameObject player;
+
+	void Update ()
+	{
+		if (!player)
+		{
+			player = GameObject.Find( "Player");
+		}
+
+		if (player)
+		{
+			Vector3 pos = transform.position;
+
+			pos.x = player.transform.position.x;
+			pos.y = player.transform.position.y;
+				
+			transform.position = pos;
+		}
+	}
+}
